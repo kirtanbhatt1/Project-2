5637de802a3d7fd8bf9ea0d0ddca83ffa8e16526
kirtanbhatt1 <knbhatt104@gmail.com>
2022-10-30 22:54:07 -0700

    small addition

---
+++ NFLProject/Database.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "NFLProject.h"
+#include <map>
+
+class Database
+{
+
+};
+
---
+++ NFLProject/Team.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "NFLProject.h"
+
+class Team
+{
+public:
+	QString teamName;
+	QString stadiumName;
+	int capacity;
+	QString location;
+	QString roofType;
+	QString surface;
+	int openedYear;
+	QString conference;
+	QString division;
+
+
+};
