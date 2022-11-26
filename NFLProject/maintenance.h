2068f93a831e2800975ccc3cbde4e94efd7b1786
Ericcccccai <102574544+Ericcccccai@users.noreply.github.com>
2022-11-26 14:57:15 -0800

    Update maintenance.h

--- NFLProject/maintenance.h
+++ NFLProject/maintenance.h
@@ -2,6 +2,10 @@
 #define MAINTENANCE_H
 
 #include <QMainWindow>
+#include <iostream>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QDebug>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Maintenance; }
@@ -14,6 +18,22 @@ class Maintenance : public QMainWindow
 public:
     Maintenance(QWidget *parent = nullptr);
     ~Maintenance();
+    QSqlDatabase NFLDatabase;
+
+private slots:
+    void on_addTeamButton_clicked();
+
+    void on_changePriceButton_clicked();
+
+    void on_addSouvenirButton_clicked();
+
+    void on_deleteSouvenirButton_clicked();
+
+    void on_teamSelectSouveirCombo_currentTextChanged(const QString &arg1);
+
+    void on_selectSouvenirCombo_currentTextChanged(const QString &arg1);
+
+    void on_changeTeamInfoButton_clicked();
 
 private:
     Ui::Maintenance *ui;
